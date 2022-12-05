47323f3b8a1fdec72c501fa7aa7aff4bc5f07984
Tomi Tokko <56736462+tomitokko@users.noreply.github.com>
2022-12-05 20:34:54 +0000

    Update settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 #     'default': dj_database_url.config()
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
