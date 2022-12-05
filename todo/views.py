df50cd7b5f4112b3edb98b86c233145803fa01ab
Tomi Tokko <56736462+tomitokko@users.noreply.github.com>
2022-12-05 20:44:49 +0000

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
-from .models import Todo
+# from .models import Todo
 
 # Create your views here.
 def index(request):
-    todo = Todo.objects.all()
-    if request.method == 'POST':
-        new_todo = Todo(
-            title = request.POST['title']
-        )
-        new_todo.save()
-        return redirect('/')
+#     todo = Todo.objects.all()
+#     if request.method == 'POST':
+#         new_todo = Todo(
+#             title = request.POST['title']
+#         )
+#         new_todo.save()
+#         return redirect('/')
 
-    return render(request, 'index.html', {'todos': todo})
+    return render(request, 'index.html')
 
 def delete(request, pk):
-    todo = Todo.objects.get(id=pk)
-    todo.delete()
+#     todo = Todo.objects.get(id=pk)
+#     todo.delete()
     return redirect('/')
